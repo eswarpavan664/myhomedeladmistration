6b8a42f5ea67cf4f2f10e288431b68a2eab9b545
pavaneswar kothalanka <itonystark58@gmail.com>
2022-12-03 19:50:39 +0530

    first commit1234545512

--- src/constants/Ip.js
+++ src/constants/Ip.js
@@ -1,4 +1,4 @@
-export const Ip = "https://myhomedel.herokuapp.com";
+export const Ip = "https://sore-ruby-scarab-boot.cyclic.app";
 
 // "https://myhomedel.herokuapp.com" 
-//http://192.168.28.237:5000
+//http://192.168.28.237:5000s
